54d4e0dbe7e7e22184fef9540aa8b2ac8113930c
Márcio Ramos Medeiros <80805761+MarcioRMed@users.noreply.github.com>
2022-01-19 17:17:58 -0300

    inicio estudo React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Primeiro from './components/Primeiro'
+import ComParamentro from './components/ComParamentro'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    
+    <div>
+        <Primeiro />
+        
+        <ComParamentro titulo="Esse é o título"
+                       subtitulo="Esse é o subtítulo"/>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        <ComParamentro titulo="Opa"
+                       subtitulo="Epa"/>
+     </div>,
+        document.getElementById('root')
+)
